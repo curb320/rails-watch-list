f11f9981af37b9cd1e8bb2043b6b04547e039878
Vincent Demaret <demaretvincent001@gmail.com>
2024-11-16 12:40:39 +0100

    chatGPT mis en forme l'index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,10 +14,14 @@ class ListsController < ApplicationController
   end
   def create
     @list = List.create(list_params)
-    # redirect_to lists_path
     redirect_to root_path
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to root_path
+  end
   private
 
   def list_params
